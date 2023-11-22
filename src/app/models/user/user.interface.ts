57acc9b9105f2e1df22f121ccc9efcb3c7e1d8e7
Rabby Khan <ajrabbyK72@gmail.com>
2023-11-22 23:58:30 +0600

    Created user interface

---
+++ src/app/models/user/user.interface.ts
@@ -0,0 +1,33 @@
+// FullName Interface
+export type TFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+// Address Interface
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+// Order Interface
+export type TOrder = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+// User Interface
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: TFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: TAddress;
+  orders?: TOrder[];
+};
