b9df7c836b073926cefea4c5d4c7872cb4412d29
Rabby Khan <ajrabbyK72@gmail.com>
2023-11-22 23:25:09 +0600

    Added prettier and eslint formater

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-const app = express();
+const app: Application = express();
 
-const port = 3000;
+// parsers
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world!!");
