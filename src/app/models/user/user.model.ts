69065dfaae56811423bed3df0d263269e72e9588
Rabby Khan <ajrabbyK72@gmail.com>
2023-11-23 00:04:38 +0600

    Created user model

---
+++ src/app/models/user/user.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+import { TAddress, TFullName, TOrder, TUser } from "./user.interface";
+
+// Create a Schema corresponding to the document interface.
+
+// fullName Schema
+const fullNameSchema = new Schema<TFullName>({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+// address Schema
+const addressSchema = new Schema<TAddress>({
+  street: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+// order Schema
+const orderSchema = new Schema<TOrder>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+});
+
+// user Schema
+const userSchema = new Schema<TUser>({
+  userId: { type: Number, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  fullName: { type: fullNameSchema, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: { type: addressSchema, required: true },
+  orders: { type: [orderSchema] },
+});
+
+// Create a Model.
+export const user = model<TUser>("user", userSchema);
